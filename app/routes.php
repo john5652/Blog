4f4c17796443e872cef1e5607b2f5dd24b96b1d0
Johnathan Christopherson <johnathan.christopherson@live.com>
2015-09-03 13:59:26 -0500

    fixing

--- app/routes.php
+++ app/routes.php
@@ -31,10 +31,7 @@ Route::resource('items', 'ItemsController');
 Route::get('create', 'ItemsController@characterCreate');
 Route::get('usersCreate', 'ItemsController@userCreate');
 Route::get('blogUsersCreate', 'PostsController@userCreate');
-Route::get('/portfolio', function()
-{
-    return "This is my portfolio!";
-});
+
 Route::get('login', 'HomeController@showLogin'); 
 Route::post('login', 'HomeController@doLogin'); 
 Route::post('resume_contact', 'HomeController@sendEmail'); 
@@ -59,17 +56,3 @@ Route::get('/rolldice/{guess}', function($guess)
 	return View::make('rolldice')->with($data);
 });
 
-Route::get('orm-test', function ()
-{
-	
-$post1 = new Post();
-$post1->title = 'Eloquent is awesome!';
-$post1->body  = 'It is super easy to create a new post.';
-$post1->save();
-
-$post2 = new Post();
-$post2->title = 'Post number two';
-$post2->body  = 'The body for post number two.';
-$post2->save();
-
-});
